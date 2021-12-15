5ddb3d227f1f54be3039cfb9a2eeb8ff27410971
Junichi Kakisako <kilo.sw@gmail.com>
2021-12-15 22:39:31 +0900

    Specify binding address and port

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use clap::{crate_authors, crate_description, crate_name, crate_version, value_t_or_exit, Arg};
 
 async fn index(req: HttpRequest) -> impl Responder {
     if let Some(ip) = req.connection_info().realip_remote_addr() {
@@ -12,8 +13,32 @@ async fn index(req: HttpRequest) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let matches = clap::App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("binding")
+                .long("binding")
+                .short("b")
+                .default_value("127.0.0.1")
+                .help("Binds server to the specified IP"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .long("port")
+                .short("p")
+                .default_value("8080")
+                .help("Runs server on the specified port"),
+        )
+        .get_matches();
+
+    let binding = value_t_or_exit!(matches.value_of("binding"), String);
+    let port = value_t_or_exit!(matches.value_of("port"), u16);
+
+    println!("Server started {}:{}", binding, port);
     HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind(("127.0.0.1", 8080))?
+        .bind((binding, port))?
         .run()
         .await
 }
